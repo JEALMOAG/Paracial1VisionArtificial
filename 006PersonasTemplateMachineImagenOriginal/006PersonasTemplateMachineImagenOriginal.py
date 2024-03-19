6ecf40726a57cf37853175fd3ecdcdb2eb2c9c29
JEALMOAG <a21310118@ceti.mx>
2024-03-19 10:17:38 -0600

    Reconocimiento de Objetos dentro de una imagen
    Template Machine, algoritmo que permite encontrar objetos dentro de imagenes.

---
+++ 006PersonasTemplateMachineImagenOriginal/006PersonasTemplateMachineImagenOriginal.py
@@ -0,0 +1,31 @@
+import cv2
+image = cv2.imread("personas.jpg")
+#image = imagenOriginal.copy()
+template = cv2.imread("template1.jpg")
+
+
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+#cv2.imshow('personasGris', image_gray)
+
+methods = [cv2.TM_SQDIFF,cv2.TM_SQDIFF_NORMED,cv2.TM_CCORR,cv2.TM_CCOEFF_NORMED\
+           ,cv2.TM_CCOEFF,cv2.TM_CCOEFF_NORMED]
+for method in methods:
+    res = cv2.matchTemplate(image_gray, template_gray, method=method)
+
+    min_val,max_val,min_loc,max_loc = cv2.minMaxLoc(res)
+    print(min_val,max_val,min_loc,max_loc)
+    if method == cv2.TM_SQDIFF or cv2.TM_SQDIFF_NORMED :
+        x1,y1 = min_loc
+        x2,y2 = min_loc[0] + template.shape[1], min_loc[1] + template.shape[0]
+    else:
+      x1,y1 = max_loc
+      x2,y2 = max_loc[0] + template.shape[1],max_loc[1] + template.shape[0]   
+    
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    cv2.imshow("Image", image)
+    cv2.imshow("Template", template)
+ #   image_gray = imagenOriginal.copy()
+    cv2.waitKey(0)
+cv2.destroyAllWindows()
